89c723378779668d1071ca36cc5b9cc8821fa9cc
Toffee <toffee24@foxmail.com>
2018-04-04 11:12:54 +0800

    build构建

---
+++ prod.server.js
@@ -0,0 +1,92 @@
+var express = require('express')
+var axios = require('axios')
+
+var app = express()
+var apiRoutes = express.Router()
+
+app.get('/api/getDiscList', function (req, res) {
+  const url = 'https://c.y.qq.com/splcloud/fcgi-bin/fcg_get_diss_by_tag.fcg'
+  axios.get(url, {
+    headers: {
+      referer: 'https://c.y.qq.com/',
+      host: 'c.y.qq.com'
+    },
+    params: req.query
+  }).then((response) => {
+    res.json(response.data)
+  }).catch((e) => {
+    console.log(e)
+  })
+})
+app.get('/api/lyric', function (req, res) {
+  const url = 'https://c.y.qq.com/lyric/fcgi-bin/fcg_query_lyric_new.fcg'
+  axios.get(url, {
+    headers: {
+      referer: 'https://c.y.qq.com/',
+      host: 'c.y.qq.com'
+    },
+    params: req.query
+  }).then((response) => {
+    let ret = response.data
+    if (typeof ret === 'string') {
+      let reg = /^\w+\(({[^()]+})\)$/
+      let mathes = ret.match(reg)
+      if (mathes) {
+        ret = JSON.parse(mathes[1])
+      }
+    }
+    res.json(ret)
+  }).catch((e) => {
+    console.log(e)
+  })
+})
+app.get('/api/getDissList', function (req, res) {
+  const url = 'https://c.y.qq.com/qzone/fcg-bin/fcg_ucc_getcdinfo_byids_cp.fcg'
+  axios.get(url, {
+    headers: {
+      referer: 'https://c.y.qq.com/',
+      host: 'c.y.qq.com'
+    },
+    params: req.query
+  }).then((response) => {
+    let ret = response.data
+    if (typeof ret === 'string') {
+      let reg = /^\w+\(({[^()]+})\)$/
+      let mathes = ret.match(reg)
+      if (mathes) {
+        ret = JSON.parse(mathes[1])
+      }
+    }
+    res.json(ret)
+  }).catch((e) => {
+    console.log(e)
+  })
+})
+app.get('/api/getRankList', function (req, res) {
+  const url = 'https://c.y.qq.com/v8/fcg-bin/fcg_myqq_toplist.fcg'
+  axios.get(url, {
+    headers: {
+      referer: 'https://c.y.qq.com/',
+      host: 'c.y.qq.com'
+    },
+    params: req.query
+  }).then((response) => {
+    res.json(response.data)
+  }).catch((e) => {
+    console.log(e)
+  })
+})
+
+// app.use('/api', apiRoutes)
+
+app.use(express.static('./dist'))
+
+var port = 9000
+
+module.exports = app.listen(port, function (err) {
+  if (err) {
+    console.log(err)
+    return
+  }
+  console.log('listening at http://localhost:' + port + '\n')
+})
